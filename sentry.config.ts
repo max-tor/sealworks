6113600d6ad89da808d37cb9f2f7e0cb11023cd0
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2024-09-08 22:49:22 +0200

    feat: enable debug mode in Sentry configuration

--- sentry.config.ts
+++ sentry.config.ts
@@ -4,6 +4,8 @@ console.log('Sentry.init');
 
 Sentry.init({
   dsn: 'https://a5f2d1f880f43191a0f7045fcdfb0a3b@o1363374.ingest.us.sentry.io/4507916934184960',
+  debug: true,
+  replaysSessionSampleRate: 0,
   integrations: [
     Sentry.feedbackIntegration({
       enableScreenshot: true,
