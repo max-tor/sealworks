7b4c0533b0eebfb1db2d0150841ce364fd45aaa7
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2024-09-08 18:20:56 +0200

    fix: enhance sentry feedback with screenshot support

--- sentry.config.ts
+++ sentry.config.ts
@@ -1,10 +1,13 @@
 import * as Sentry from '@sentry/gatsby';
 
+console.log('Sentry.init');
+
 Sentry.init({
   dsn: 'https://a5f2d1f880f43191a0f7045fcdfb0a3b@o1363374.ingest.us.sentry.io/4507916934184960',
   integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.feedbackIntegration(),
+    Sentry.feedbackIntegration({
+      enableScreenshot: true,
+    }),
   ],
   // Tracing
   tracesSampleRate: 0,
