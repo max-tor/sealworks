bcbe1489959dcb1990c002daf30408c21c3ab0f4
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2024-09-11 12:41:58 +0200

    feat: add a button

---
+++ src/components/common/CalendlyDialog/index.ts
@@ -0,0 +1,1 @@
+export { CalendlyDialog } from './CalendlyDialog';
--- src/hooks/useCalendlyWidget.tsx
+++ src/hooks/useCalendlyWidget.tsx
@@ -1,10 +1,7 @@
-import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
 import { Button } from 'flowbite-react';
 import React, { useState } from 'react';
-import { InlineWidget, useCalendlyEventListener } from 'react-calendly';
-import { PageHeightResizeEvent } from 'react-calendly/typings/components/hooks/useCalendlyEventListener';
 
-import { calendlySettings, calendlyUser } from '@/configs';
+import { CalendlyDialog } from '@/components/common/CalendlyDialog';
 
 type CalendlyWidgetProps = {
   label?: string;
@@ -14,14 +11,7 @@ export const useCalendlyWidget = ({
   label = 'Schedule time with us',
 }: CalendlyWidgetProps) => {
   const [open, setOpen] = useState(false);
-  const [height, setHeight] = useState('500px');
-  const onPageHeightResize = (e: PageHeightResizeEvent) => {
-    setHeight(e.data.payload.height);
-  };
 
-  useCalendlyEventListener({
-    onPageHeightResize,
-  });
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -35,21 +25,10 @@ export const useCalendlyWidget = ({
           {label}
         </Button>
       </div>
-      <Dialog open={open} onClose={handleClose} className="relative z-50">
-        <DialogBackdrop className="fixed inset-0 z-40 bg-black/75" />
-
-        <div className="fixed inset-0 z-50 w-screen overflow-y-auto p-4">
-          <div className="fixed inset-0 flex w-screen items-center justify-center">
-            <DialogPanel className="w-auto max-w-lg flex-[1_1_860px]">
-              <InlineWidget
-                url={`https://calendly.com/${calendlyUser}`}
-                pageSettings={calendlySettings}
-                styles={{ height }}
-              />
-            </DialogPanel>
-          </div>
-        </div>
-      </Dialog>
+      <CalendlyDialog
+        open={open}
+        onClose={handleClose}
+      />
     </>
   );
 };
