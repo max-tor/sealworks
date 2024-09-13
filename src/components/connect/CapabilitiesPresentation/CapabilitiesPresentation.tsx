d348d2b6b246eb5a6e13d0d028c00f435376ce24
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2024-09-13 14:28:26 +0200

    chore: remove clsx import and inline static classes
    Refactor to use inline class names instead of the wrapperClasses variable.
    This simplifies code and eliminates unnecessary clsx dependency.

--- src/components/connect/CapabilitiesPresentation/CapabilitiesPresentation.tsx
+++ src/components/connect/CapabilitiesPresentation/CapabilitiesPresentation.tsx
@@ -1,6 +1,5 @@
 import 'react-multi-carousel/lib/styles.css';
 
-import clsx from 'clsx';
 import { BreakpointsObject, useBreakpoint } from 'gatsby-plugin-breakpoints';
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
@@ -33,9 +32,6 @@ const ResponsiveCarousel = ({
   return <Carousel responsive={responsive}>{children}</Carousel>;
 };
 
-const wrapperClasses =
-  'lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover';
-
 export const CapabilitiesPresentation = () => {
   const breakpoints = useBreakpoint();
 
@@ -47,7 +43,7 @@ export const CapabilitiesPresentation = () => {
     <section className="relative z-10 my-5 lg:h-[496] lg:overflow-hidden xl:mb-0 xl:pb-5">
       <ResponsiveCarousel breakpoints={breakpoints}>
         <StaticImage
-          className={clsx(wrapperClasses, '')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Case study on how Ellen's credit score affects her ability to buy a house."
           src="images/ellen-credit-score-strategy.jpg"
@@ -56,7 +52,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[295px] xl:top-0')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[295px] xl:top-0 xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto xl:max-w-full object-top"
           alt="Diversity and Inclusion course slide explaining the definition of diversity."
           src="images/diversity-inclusion-definition.jpg"
@@ -65,7 +61,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[582px] xl:top-0')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[582px] xl:top-0 xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Lifeguarding module pre-assessment results screen."
           src="images/lifeguarding-pre-assessment-results.jpg"
@@ -74,7 +70,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[871px] xl:top-0')}
+          className="xl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[871px] xl:top-0 xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Money Matters program facilitator dashboard with plans and activities."
           src="images/money-matters-facilitator-dashboard.jpg"
@@ -83,7 +79,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[86px] xl:top-[255px]')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[86px] xl:top-[255px] xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Moderna onboarding page with resources for new and existing employees"
           src="images/moderna-new-employee-onboarding.jpg"
@@ -92,7 +88,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[370px] xl:top-[255px]')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[370px] xl:top-[255px] xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="DEEP learning platform overview with modules on leadership and growth"
           src="images/deep-learning-platform.jpg"
@@ -101,7 +97,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[658px] xl:top-[255px]')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[658px] xl:top-[255px] xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Everboarding platform for Daiichi Sankyo’s Global R&D with learning journeys and spotlight sections."
           src="images/everboarding-global-rd.jpg"
@@ -110,7 +106,7 @@ export const CapabilitiesPresentation = () => {
         />
 
         <StaticImage
-          className={clsx(wrapperClasses, 'xl:left-[944px] xl:top-[255px]')}
+          className="lxl:shadow-gray10 block size-full object-cover xl:!absolute xl:left-[944px] xl:top-[255px] xl:h-[231px] xl:w-[256px] xl:border xl:border-gainsboro xl:object-cover"
           imgClassName="h-auto max-w-full object-top"
           alt="Nielsen Diversity and Inclusion foundational learning course introduction."
           src="images/ielsen-diversity-inclusion-course.jpg"
