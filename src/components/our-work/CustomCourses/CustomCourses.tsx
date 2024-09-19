92653c1bed2f1266da5e2647b90a836c94b66300
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2024-09-19 23:00:57 +0200

    fix: replace static image data with dynamic configurations

--- src/components/our-work/CustomCourses/CustomCourses.tsx
+++ src/components/our-work/CustomCourses/CustomCourses.tsx
@@ -9,7 +9,68 @@ const description1 =
 const description2 =
   'Our team collaborates with you to design engaging, effective learning experiences that resonate with your users, incorporating interactivity that makes educational experiences meaningful. By prioritizing your input and feedback, we ensure the final product aligns perfectly with your vision and delivers maximum impact.';
 
-export const CustomCourses: React.FC = () => {
+const blockWidth = 707;
+const blockHeight = 636;
+
+// images/young-family-scenario
+const img1 = {
+  top: 40,
+  left: 48,
+  width: 472,
+  height: 312,
+  alt: "Interactive training module featuring a floor plan and a family scenario for childcare, showing rooms with Click n'Learn and scenario activities.",
+};
+// images/girl.png
+const img2 = {
+  top: 36,
+  left: -7,
+  width: 130,
+  height: 340,
+  alt: '',
+};
+
+// images/childcare-scenario-action.png
+const img3 = {
+  top: -30,
+  left: 456,
+  width: 242,
+  height: 177,
+  alt: 'Childcare training module featuring a child with a list of actions to handle a temper tantrum.',
+};
+
+// images/reflecting-on-privilege
+const img4 = {
+  top: 408,
+  left: 91,
+  width: 400,
+  height: 238,
+  alt: 'Module on reflecting on privilege, showing questions about family background and a mountain graphic illustrating privilege levels.',
+};
+// defining-diversity-module
+const img5 = {
+  top: 383,
+  left: 15,
+  width: 100,
+  height: 206,
+  alt: 'Diversity training module defining diversity with various interactive elements and definitions.',
+};
+
+// credit-uses-example
+const img6 = {
+  top: 312,
+  left: 412,
+  width: 325,
+  height: 256,
+  alt: 'Interactive module showing common uses of credit with options to select different real-life scenarios.',
+};
+
+// showImagesInfo({
+//   blockWidth,
+//   blockHeight,
+//   images: [img1, img2, img3, img4, img5, img6],
+// });
+
+export const CustomCourses = () => {
   return (
     <section className="my-5 flex max-w-full flex-row-reverse gap-5 max-lg:flex-col lg:my-14 lg:gap-[40px] xl:gap-[116px]">
       <div className="flex-[1_1_36%] flex-col max-lg:ml-0 max-lg:w-full">
@@ -22,15 +83,66 @@ export const CustomCourses: React.FC = () => {
           <p className="mt-5">{description2}</p>
         </div>
       </div>
-      <aside className="lmy-5 relative mx-auto grid w-full gap-4 max-lg:ml-0 max-lg:mt-10 max-lg:w-full max-lg:max-w-full lg:my-14 lg:flex-[1_0_712px] lg:flex-col">
+      <aside className="lmy-5 relative mx-auto mr-[4.243281471004243%] mt-[5.597014925373134%] grid h-[89.95756718528996vw] w-full gap-4 bg-blue max-lg:ml-0 max-lg:mt-10 max-lg:w-full max-lg:max-w-full lg:my-14 lg:h-[636px] lg:w-[707px] lg:flex-[1_0_707px] lg:flex-col">
         <StaticImage
-          className="lg:h-[676px] lg:w-[744px]"
+          className="!absolute left-[6.789250353606789%] top-[7.462686567164178%] h-[58.2089552238806%] w-[66.76096181046675%] object-cover"
           imgClassName="h-auto max-w-full"
-          alt=""
-          src="images/custom-courses.png"
-          breakpoints={[640, 744, 768, 1024, 1280]}
-          sizes="(min-width: 1280px) 744px, 100vw"
+          alt={img1.alt}
+          src="images/young-family-scenario.png"
+          breakpoints={[img1.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 472px, 100vw"
         />
+        <StaticImage
+          className="!absolute -left-[0.9900990099009901%] top-[6.7164179104477615%] h-[63.43283582089553%] w-[18.387553041018386%] select-none object-cover"
+          imgClassName="h-auto max-w-full"
+          alt={img1.alt}
+          src="images/girl.png"
+          breakpoints={[img2.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 130px, 100vw"
+        />
+        <StaticImage
+          className="!absolute -top-[5.597014925373134%] left-[64.49787835926449%] h-[33.02238805970149%] w-[34.22913719943423%] object-cover"
+          imgClassName="h-auto max-w-full"
+          alt={img3.alt}
+          src="images/childcare-scenario-action.png"
+          breakpoints={[img3.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 242px, 100vw"
+        />
+        <StaticImage
+          className="!absolute left-[12.871287128712872%] top-[64.15094339622641%] h-[37.42138364779874%] w-[56.57708628005658%] object-cover"
+          imgClassName="h-auto max-w-full"
+          alt={img4.alt}
+          src="images/reflecting-on-privilege.png"
+          breakpoints={[img4.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 400px, 100vw"
+        />
+        <StaticImage
+          className="!absolute left-[2.1216407355021216%] top-[60.22012578616353%] h-[32.38993710691824%] w-[14.144271570014144%] object-cover"
+          imgClassName="h-auto max-w-full"
+          alt={img5.alt}
+          src="images/defining-diversity-module.png"
+          breakpoints={[img5.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 100px, 100vw"
+        />
+        <StaticImage
+          className="!absolute left-[58.27439886845828%] top-[49.056603773584904%] h-[40.25157232704403%] w-[45.96888260254597%] object-cover"
+          imgClassName="h-auto max-w-full"
+          alt={img6.alt}
+          src="images/credit-uses-example.png"
+          breakpoints={[img5.width, 640, 768, 1024]}
+          sizes="(min-width: 1024px) 325px, 100vw"
+        />
+        {/*
+        <div className="absolute right-0 top-[312px] w-[200px] lg:left-[412px] lg:right-auto lg:h-[256px] lg:w-[325px]">
+          <StaticImage
+            className="lg:h-[256px] lg:w-[325px] lg:object-cover lg:shadow-lg"
+            imgClassName="h-auto max-w-full lg:object-top"
+            alt="Interactive module showing common uses of credit with options to select different real-life scenarios."
+            src="images/credit-uses-example.jpg"
+            breakpoints={[325, 640, 768, 1024]}
+            sizes="(min-width: 1024px) 325px, 100vw"
+          />
+        </div>*/}
       </aside>
     </section>
   );
---
+++ src/utils.ts
@@ -0,0 +1,64 @@
+interface ImageData {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+  alt?: string;
+}
+export const getX = (name: string, px: number, blockWidth: number) =>
+  `${px < 0 ? '-' : ''}${name}-[${(Math.abs(px) / blockWidth) * 100}%]`;
+export const getY = (name: string, py: number, blockHeight: number) =>
+  `${py < 0 ? '-' : ''}${name}-[${(Math.abs(py) / blockHeight) * 100}%]`;
+
+export const getContainerClasses = ({
+  blockWidth,
+  blockHeight,
+}: {
+  blockWidth: number;
+  blockHeight: number;
+}) =>
+  `relative h-[${(blockHeight / blockWidth) * 100}vw] lg:h-[${blockHeight}px] lg:w-[${blockWidth}px]`;
+
+export const getImgClasses = ({
+  top,
+  left,
+  width,
+  height,
+  blockWidth,
+  blockHeight,
+}: {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+  blockWidth: number;
+  blockHeight: number;
+}) =>
+  `!absolute ${getX('left', left, blockWidth)} ${getY('top', top, blockHeight)} ${getX('w', width, blockWidth)} ${getY('h', height, blockHeight)} object-cover`;
+
+export const showImagesInfo = ({
+  blockWidth,
+  blockHeight,
+  images,
+}: {
+  blockWidth: number;
+  blockHeight: number;
+  images: ImageData[];
+}) => {
+  console.log({ container: getContainerClasses({ blockWidth, blockHeight }) });
+  images.forEach(({ top, left, width, height }, index) => {
+    console.log({
+      index,
+      image: getImgClasses({
+        top,
+        left,
+        width,
+        height,
+        blockWidth,
+        blockHeight,
+      }),
+    });
+  });
+};
+
+export const getBreakpoints = (width: number) => [width, 640, 768, 1024];
